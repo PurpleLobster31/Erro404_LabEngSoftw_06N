54ced23b8f76ca674828cd013408aeef64eaffba
MateusTMagalhaes <159064680+matt-teles@users.noreply.github.com>
2026-04-23 11:35:32 -0300

    feat: add Atendimento model and relationships, update domain class diagram

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, Index, LargeBinary
+from sqlalchemy import Column, Integer, String, Float, Index, LargeBinary, ForeignKey, DateTime, Date
+from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 from backend.app.database import Base
 
@@ -22,6 +23,11 @@ class Unidade(Base):
         Index('idx_unidades_localizacao', 'localizacao', postgresql_using='gist'),
     )
 
+    # Relacionamentos
+
+    # Lado "Um" do relacionamento (Uma unidade tem muitos atendimentos)
+    atendimentos = relationship("Atendimento", back_populates="unidade")
+
 class Paciente(Base):
     __tablename__ = "pacientes"
 
@@ -29,3 +35,26 @@ class Paciente(Base):
     nome = Column(String, nullable=False)
     sobrenome = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+    data_nascimento = Column(Date, nullable=True)
+
+    # Relacionamentos
+    # Lado "Um" do relacionamento
+    atendimentos = relationship("Atendimento", back_populates="paciente")
+    
+
+class Atendimento(Base):
+    __tablename__ = "atendimentos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    # nullable=False garante a obrigatoriedade no banco
+    unidade_id = Column(Integer, ForeignKey("unidades.id"), nullable=False)
+    paciente_id = Column(Integer, ForeignKey("pacientes.id"), nullable=False)
+    status = Column(String, nullable=False)
+    horario_chegada = Column(DateTime, nullable=True)
+    horario_triagem = Column(DateTime, nullable=True)
+    horario_atendimento = Column(DateTime, nullable=True)
+
+    # relacionamentos
+    # Lado "Muitos" do relacionamento (Muitos atendimentos pertencem a uma unidade)
+    unidade = relationship("Unidade", back_populates="atendimentos")
+    paciente = relationship("Paciente", back_populates="atendimentos")
